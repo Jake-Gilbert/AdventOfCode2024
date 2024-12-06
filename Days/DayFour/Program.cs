bc0b483ad4708a41c87625e9076d8cee8856428f
Jake Gilbert <jakegilbert@blueyonder.co.uk>
2024-12-06 11:03:10 +0000

    Solutions for days Three and Four

---
+++ Days/DayFour/Program.cs
@@ -0,0 +1,117 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+var grid = File.ReadAllLines("TestData/ChallengeData.TXT");
+var xmasCount = 0;
+var crossCount = 0;
+var width = grid.First().Length;
+var wordToMatch = "AMX";
+var PartTwoMatch = "MAS";
+var targetIndex = 0;
+
+
+//PartOne
+/*for (int i = 0; i < grid.Length; i++)
+{
+    targetIndex = grid[i].IndexOf("S");
+    while (targetIndex >= 0 && targetIndex < width)
+    {
+            xmasCount += ConfirmMatch(i, targetIndex, 1, 0, new List<string> { wordToMatch });
+
+            xmasCount += ConfirmMatch(i, targetIndex, -1, 0, new List<string> { wordToMatch });
+
+            xmasCount += ConfirmMatch(i, targetIndex, 0, -1, new List<string> { wordToMatch });
+
+            xmasCount += ConfirmMatch(i, targetIndex, -1, -1, new List<string> { wordToMatch });
+
+            xmasCount += ConfirmMatch(i, targetIndex, 1, -1, new List<string> { wordToMatch });
+
+            xmasCount += ConfirmMatch(i, targetIndex, 0, 1, new List<string> { wordToMatch });
+
+            xmasCount += ConfirmMatch(i, targetIndex, -1, 1, new List<string> { wordToMatch });
+
+            xmasCount += ConfirmMatch(i, targetIndex, 1, 1, new List<string> { wordToMatch });
+
+            targetIndex = grid[i].IndexOf("A", targetIndex + 1);
+    }
+    Console.WriteLine($"Christmas count - {xmasCount}");
+
+}*/
+
+for (int i = 0; i < grid.Length; i++)
+{
+    targetIndex = grid[i].IndexOf("A");
+    while (targetIndex >= 0 && targetIndex < width)
+    {
+        if (i - 1 < 0 || i + 1 >= grid.Length)
+        {
+            break;
+        }
+        else
+        {
+            crossCount += ConfirmCross(i, targetIndex) ? 1 : 0;
+        }
+        targetIndex = grid[i].IndexOf("A", targetIndex + 1);
+    }
+}
+
+    Console.WriteLine($"Cross count - {crossCount}");
+
+    //PartOne
+    /*int ConfirmMatch(int row, int col, int offsetX, int offsetY, List<string> words)
+    {
+        foreach (var letter in words.First())
+        {
+            row += offsetY;
+            col += offsetX;
+            if (row < 0 || row >= grid.Length || col < 0 || col >= grid.Length)
+            {
+                return 0;
+            }
+
+            if (grid[row][col] != letter)
+            {
+                return 0;
+            }
+        }
+        return 1;
+    }*/
+
+    bool ConfirmCross(int row, int col)
+    {
+        var oppositeCount = 0;
+        if (col + 1 >= width || col - 1 < 0)
+        {
+            return false;
+        }
+        if (grid[row - 1][col + 1] == 'M')
+        {
+            oppositeCount += CheckForOpposite(row + 1, col - 1, 'M') ? 1 : 0;
+        }
+        else if (grid[row - 1][col + 1] == 'S')
+        {
+            oppositeCount += CheckForOpposite(row + 1, col - 1, 'S') ? 1 : 0;
+        }
+        if (grid[row - 1][col -1] == 'M')
+        {
+            oppositeCount += CheckForOpposite(row + 1, col + 1, 'M') ? 1 : 0;
+        }
+        else if (grid[row - 1][col - 1] == 'S')
+        {
+            oppositeCount += CheckForOpposite(row + 1, col + 1, 'S') ? 1 : 0;
+        }
+        return oppositeCount == 2;
+    }
+
+    bool CheckForOpposite(int targetRow, int targetCol, char currentChar)
+    {
+        if (grid[targetRow][targetCol] == 'S' && currentChar == 'M')
+        {
+            return true;
+        }
+        if (grid[targetRow][targetCol] == 'M' && currentChar == 'S')
+        {
+            return true;
+        }
+        return false;
+    }
---
+++ Days/DayThree/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Security.Principal;
+using static System.Net.Mime.MediaTypeNames;
+
+var file = File.ReadAllText("TestData\\ChallengeData.TXT");
+var indexOne = 0;
+var indexTwo = 0;
+var count = 0;
+
+while (true)
+{
+    indexTwo = file.IndexOf("don't()", indexOne);
+    indexOne = file.IndexOf("mul(", indexOne);
+
+    if (indexOne == -1)
+    {
+        break;
+    }
+    else if (indexTwo < indexOne && indexTwo > 0)
+    {
+        indexOne = file.IndexOf("do()", indexTwo);
+        if (indexOne < 1)
+        {
+            break;
+        }
+        continue;
+    }
+    else
+    {
+        indexOne += 4;
+        if (AllNumbers(ref indexOne, out var leftNum))
+        {
+            if (file[indexOne] == ',')
+            {
+                indexOne++;
+                if (AllNumbers(ref indexOne, out var rightNum))
+                {
+                    if (file[indexOne] == ')')
+                    {
+                        count += leftNum * rightNum;
+                    }
+                }
+            }
+        }
+    }
+}
+   
+
+Console.WriteLine($"Count - {count}");
+
+bool AllNumbers(ref int idx, out int number)
+{
+    var result = false;
+    var numStr = "";
+    while (Char.IsNumber(file[idx]))
+    {
+        numStr += file[idx];
+        idx++;
+        result = true;
+    }
+
+    number = int.Parse(numStr);
+    return result;
+}
